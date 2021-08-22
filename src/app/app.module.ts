2ca355b11de201630a2c8c19834b300dad2c1c07
Maciel Barbosa <macielbarbosa@outlook.com>
2021-08-22 20:29:02 -0300

    feature: listagem das pessoas cadastradas #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PessoasComponent } from './pessoas/pessoas.component';
 import { PessoaComponent } from './pessoa/pessoa.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PessoasComponent,
-    PessoaComponent
-  ],
+  declarations: [AppComponent, PessoasComponent, PessoaComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pessoas/pessoas.component.ts
+++ src/app/pessoas/pessoas.component.ts
@@ -1,15 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { URL_API } from 'src/utils/constants';
 
 @Component({
   selector: 'app-pessoas',
   templateUrl: './pessoas.component.html',
-  styleUrls: ['./pessoas.component.scss']
+  styleUrls: ['./pessoas.component.scss'],
 })
 export class PessoasComponent implements OnInit {
+  pessoas: any;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
+    this.http.get<any>(URL_API).subscribe((data) => {
+      this.pessoas = data;
+    });
   }
 
+  cadastrar() {
+    this.router.navigate(['/pessoa']);
+  }
+
+  editar(id: string) {
+    this.router.navigate(['/pessoa', { id }]);
+  }
 }
