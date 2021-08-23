e9c2295756c015b19029cf190528a54456eab060
Maciel Barbosa <macielbarbosa@outlook.com>
2021-08-23 00:35:52 -0300

    feature: voltar para a home ao clicar na brand #6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'desafio4asset';
+
+  constructor(private router: Router) {}
+
+  goToHome() {
+    this.router.navigate(['/']);
+  }
 }
